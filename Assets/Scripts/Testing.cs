71ae17b6e1df117a53870cc3609f1401a2445e2b
TEO-HOME\TEO <thyun@cookapps.com>
2020-07-31 01:50:00 +0900

    ECS 기본 학습 진행 : https://www.youtube.com/watch?v=C56bbgtPr_w&list=PLzDRvYVwl53s40yP5RQXitbT--IRcHqba&index=2

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class Testing : MonoBehaviour
+{
+
+    [SerializeField] Mesh mesh;
+    [SerializeField] Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(RenderBounds),
+            typeof(MoveSpeedComponent)
+        );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(10000, Allocator.Temp);
+
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for(int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelComponent { level = UnityEngine.Random.Range(10, 100) });
+            entityManager.SetComponentData(entity, new MoveSpeedComponent { moveSpeed = UnityEngine.Random.Range(1f, 2f) });
+            entityManager.SetComponentData(entity, new Translation { Value = new float3(UnityEngine.Random.Range(-8, 8f), UnityEngine.Random.Range(-8, 8f), 0f) });
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = this.mesh,
+                material = this.material,
+            });
+        }
+        entityArray.Dispose();
+    }
+
+}
